6d8344a46dd9b6b72b3deca9e349425ffab7fc57
Aidan Dodds <aidan@codeplay.com>
2020-10-11 14:01:13 +0100

    Start working on codegen

--- riscv_jit_2/ir.c
+++ riscv_jit_2/ir.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <assert.h>
 #include <string.h>
 
@@ -543,10 +544,287 @@ static int32_t eval(struct riscv_t *rv, const struct ir_inst_t *i) {
   return 0;
 }
 
+// byte offset from rv structure address to member address
+#define rv_offset(RV, MEMBER) ((int32_t)(((uintptr_t)&(RV->MEMBER)) - (uintptr_t)RV))
+
+static uint32_t reg_next(uint32_t mask) {
+  if (0 == (mask & 1)) return 1;
+  if (0 == (mask & 2)) return 2;
+  if (0 == (mask & 4)) return 4;
+  assert(!"out of registers");
+}
+
+static const char *reg_name(int32_t r) {
+  switch (r) {
+  case 1: return "eax";
+  case 2: return "ecx";
+  case 3: return "edx";
+  default:
+    assert(!"unreachable");
+    return "";
+  }
+}
+
+static void ir_codegen_alu(struct riscv_t *rv, const struct ir_inst_t *i, int32_t r, int32_t rmask, const char *op) {
+  // get the register name
+  const char *reg = reg_name(r);
+  ir_codegen(rv, i->lhs, reg, rmask);
+  const int32_t next = reg_next(rmask);
+  ir_codegen(rv, i->rhs, next, rmask);
+  printf("%s %s, %s\n", op, reg, reg_name(next));
+}
+
+static void ir_codegen_cmp(struct riscv_t *rv, const struct ir_inst_t *i, int32_t r, int32_t rmask, const char *op) {
+  // get the register name
+  const char *reg = reg_name(r);
+
+  const int32_t r1 = reg_next(rmask);
+  ir_codegen(rv, i->lhs, reg, rmask);
+
+  const int32_t next = reg_next(rmask);
+  ir_codegen(rv, i->rhs, next, rmask);
+
+  printf("xor %s, %s", reg, reg);
+  printf("cmp %s, %s\n", reg, reg_name(next));
+  switch (i->op) {
+  case 
+  }
+}
+
+static void ir_codegen(struct riscv_t *rv, const struct ir_inst_t *i, int32_t r, int32_t rmask) {
+  // add our register to the register mask so we dont reallocate it
+  rmask |= r;
+  // get the register name
+  const char *reg = reg_name(r);
+  // branch based on the opcode
+  switch (i->op) {
+  case inst_imm:
+    // return i->imm;
+    printf("mov %s, %d\n", reg, i->imm);
+    break;
+  case inst_ld_reg:
+    // return (int32_t)(rv->X[i->offset]);
+    printf("mov %s, [rsi + %u]\n", reg, rv_offset(rv, X[i->offset]));
+    return;
+  case inst_st_reg:
+    // rv->X[i->offset] = eval(rv, i->value);
+    if (i->value->op == inst_imm) {
+      printf("mov [rsi + %u], %d\n", rv_offset(rv, X[i->offset]), i->imm);
+    }
+    else {
+      ir_codegen(rv, i->value, r, rmask);
+      printf("mov [rsi + %u], %s\n", rv_offset(rv, X[i->offset]), reg);
+    }
+    break;
+  case inst_st_pc:
+    // rv->PC = eval(rv, i->value);
+    if (i->value->op == inst_imm) {
+      printf("mov [rsi + %u], %u\n", rv_offset(rv, PC), i->imm);
+    }
+    else {
+      ir_codegen(rv, i->value, reg, rmask);
+      printf("mov [rsi + %u], %s\n", rv_offset(rv, PC), reg);
+    }
+    break;
+  case inst_branch:
+//    rv->PC = eval(rv, i->cond) ? eval(rv, i->lhs) : eval(rv, i->rhs);
+    break;
+  case inst_add:
+    ir_codegen_alu(rv, i, r, rmask, "add");
+    break;
+  case inst_sub:
+    ir_codegen_alu(rv, i, r, rmask, "sub");
+    break;
+  case inst_and:
+    ir_codegen_alu(rv, i, r, rmask, "and");
+    break;
+  case inst_or:
+    ir_codegen_alu(rv, i, r, rmask, "or");
+    break;
+  case inst_xor:
+    ir_codegen_alu(rv, i, r, rmask, "xor");
+    break;
+  case inst_shr:
+    ir_codegen_alu(rv, i, r, rmask, "shr");
+    break;
+  case inst_sar:
+    ir_codegen_alu(rv, i, r, rmask, "sar");
+    break;
+  case inst_shl:
+    ir_codegen_alu(rv, i, r, rmask, "shl");
+    break;
+  case inst_mul:
+    ir_codegen_alu(rv, i, r, rmask, "mul");
+    break;
+  case inst_mulh:
+//  {
+//    const int64_t a = (int64_t)eval(rv, i->lhs);
+//    const int64_t b = (int64_t)eval(rv, i->rhs);
+//    return ((uint64_t)(a * b)) >> 32;
+//  }
+    break;
+  case inst_mulhsu:
+//  {
+//    const int64_t a = (int64_t)eval(rv, i->lhs);
+//    const uint64_t b = (uint32_t)eval(rv, i->rhs);
+//    return ((uint64_t)(a * b)) >> 32;
+//  }
+    break;
+  case inst_mulhu:
+//  {
+//    const uint64_t a = ((uint32_t)eval(rv, i->lhs));
+//    const uint64_t b = ((uint32_t)eval(rv, i->rhs));
+//    return (a * b) >> 32;
+//  }
+    break;
+  case inst_div:
+//  {
+//    const int32_t dividend = eval(rv, i->lhs);
+//    const int32_t divisor = eval(rv, i->rhs);
+//    if (divisor == 0) {
+//      return ~0u;
+//    }
+//    else if (divisor == -1 && dividend == 0x80000000u) {
+//      return dividend;
+//    }
+//    else {
+//      return dividend / divisor;
+//    }
+//  }
+    break;
+  case inst_divu:
+//  {
+//    const uint32_t dividend = eval(rv, i->lhs);
+//    const uint32_t divisor = eval(rv, i->rhs);
+//    return (divisor == 0) ? ~0u : (dividend / divisor);
+//  }
+    break;
+  case inst_rem:
+//  {
+//    const int32_t dividend = eval(rv, i->lhs);
+//    const int32_t divisor = eval(rv, i->rhs);
+//    if (divisor == 0) {
+//      return dividend;
+//    }
+//    else if (divisor == -1 && dividend == 0x80000000u) {
+//      return 0;
+//    }
+//    else {
+//      return dividend % divisor;
+//    }
+//  }
+    break;
+  case inst_remu:
+//  {
+//    const uint32_t dividend = eval(rv, i->lhs);
+//    const uint32_t divisor = eval(rv, i->rhs);
+//    return (divisor == 0) ? dividend : (dividend % divisor);
+//  }
+    break;
+  case inst_eq:
+
+//    return eval(rv, i->lhs) == eval(rv, i->rhs);
+    break;
+  case inst_neq:
+//    return eval(rv, i->lhs) != eval(rv, i->rhs);
+//  case inst_lt:
+//    return eval(rv, i->lhs) < eval(rv, i->rhs);
+    break;
+  case inst_ge:
+//    return eval(rv, i->lhs) >= eval(rv, i->rhs);
+    break;
+  case inst_ltu:
+//    return (uint32_t)eval(rv, i->lhs) < (uint32_t)eval(rv, i->rhs);
+    break;
+  case inst_geu:
+//    return (uint32_t)eval(rv, i->lhs) >= (uint32_t)eval(rv, i->rhs);
+    break;
+  case inst_sb:
+    // rv->io.mem_write_b(rv, (uint32_t)eval(rv, i->addr), eval(rv, i->value));
+    printf("mov rcx, rsi\n");         // arg1 rv
+    ir_codegen(rv, i->addr, "edx");   // arg2 addr
+    ir_codegen(rv, i->value, "eax");
+    printf("mov r8, rax\n");          // arg3 value
+    printf("call [rsi + %u]\n", rv_offset(rv, io.mem_write_b));
+    break;
+  case inst_sh:
+//    rv->io.mem_write_s(rv, (uint32_t)eval(rv, i->addr), eval(rv, i->value));
+    printf("mov rcx, rsi\n");         // arg1 rv
+    ir_codegen(rv, i->addr, "edx");   // arg2 addr
+    ir_codegen(rv, i->value, "eax");
+    printf("mov r8, rax\n");          // arg3 value
+    printf("call [rsi + %u]\n", rv_offset(rv, io.mem_write_s));
+    break;
+  case inst_sw:
+//    rv->io.mem_write_w(rv, (uint32_t)eval(rv, i->addr), eval(rv, i->value));
+    printf("mov rcx, rsi\n");         // arg1 rv
+    ir_codegen(rv, i->addr, "edx");   // arg2 addr
+    ir_codegen(rv, i->value, "eax");
+    printf("mov r8, rax\n");          // arg3 value
+    printf("call [rsi + %u]\n", rv_offset(rv, io.mem_write_w));
+    break;
+  case inst_lb:
+    // return (int8_t)rv->io.mem_read_b(rv, (uint32_t)eval(rv, i->addr));
+    printf("mov rcx, rsi\n");         // arg1 rv
+    ir_codegen(rv, i->addr, "edx");   // arg2 addr
+    ir_codegen(rv, i->value, "eax");
+    printf("mov r8, rax\n");          // arg3 value
+    printf("call [rsi + %u]\n", rv_offset(rv, io.mem_read_b));
+    printf("movsx %s, al\n", reg);
+    break;
+  case inst_lh:
+//    return (int16_t)rv->io.mem_read_s(rv, (uint32_t)eval(rv, i->addr));
+    printf("mov rcx, rsi\n");         // arg1 rv
+    ir_codegen(rv, i->addr, "edx");   // arg2 addr
+    ir_codegen(rv, i->value, "eax");
+    printf("mov r8, rax\n");          // arg3 value
+    printf("call [rsi + %u]\n", rv_offset(rv, io.mem_read_s));
+    printf("movsx %s, ax", reg);
+    break;
+  case inst_lw:
+//    return (int32_t)rv->io.mem_read_w(rv, (uint32_t)eval(rv, i->addr));
+    printf("mov rcx, rsi\n");         // arg1 rv
+    ir_codegen(rv, i->addr, "edx");   // arg2 addr
+    ir_codegen(rv, i->value, "eax");
+    printf("mov r8, rax\n");          // arg3 value
+    printf("call [rsi + %u]\n", rv_offset(rv, io.mem_read_w));
+    break;
+  case inst_lbu:
+//    return (uint8_t)rv->io.mem_read_s(rv, (uint32_t)eval(rv, i->addr));
+    printf("mov rcx, rsi\n");         // arg1 rv
+    ir_codegen(rv, i->addr, "edx");   // arg2 addr
+    ir_codegen(rv, i->value, "eax");
+    printf("mov r8, rax\n");          // arg3 value
+    printf("call [rsi + %u]\n", rv_offset(rv, io.mem_read_b));
+    break;
+  case inst_lhu:
+//    return (uint16_t)rv->io.mem_read_s(rv, (uint32_t)eval(rv, i->addr));
+    printf("mov rcx, rsi\n");         // arg1 rv
+    ir_codegen(rv, i->addr, "edx");   // arg2 addr
+    ir_codegen(rv, i->value, "eax");
+    printf("mov r8, rax\n");          // arg3 value
+    printf("call [rsi + %u]\n", rv_offset(rv, io.mem_read_s));
+    break;
+  case inst_ecall:
+//    rv->io.on_ecall(rv, 0, 0);
+    printf("mov rcx, rsi\n"); // arg1
+    printf("call [rsi + %u]\n", rv_offset(rv, io.on_ecall));
+    break;
+  case inst_ebreak:
+//    rv->io.on_ebreak(rv, 0, 0);
+    printf("mov rcx, rsi\n"); // arg1
+    printf("call [rsi + %u]\n", rv_offset(rv, io.on_ebreak));
+    break;
+  default:
+    assert(!"unreachable");
+  }
+}
+
 void ir_eval(struct ir_block_t *block, struct riscv_t *rv) {
   for (int i = 0; i < block->head; ++i) {
     const struct ir_inst_t *inst = block->inst + i;
     if (inst->parent != NULL) {
+      // XXX: we need a skip list or something
       continue;
     }
     eval(rv, inst);
